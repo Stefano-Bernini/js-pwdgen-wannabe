4c29fe0579ba5f3a24915025c7764da9b901fa11
Stefano-Bernini <stefanobernini26@gmail.com>
2023-05-17 02:14:42 +0200

    esercizio concluso

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const name = prompt("Qual e' il tuo nome?");
+const surname = prompt("Qual e' il tuo cognome?");
+const color = prompt("Qual e' il colore preferito?");
+
+const end_num = '21';
+
+document.getElementById('data_provided').innerHTML = name + surname + color + end_num;
